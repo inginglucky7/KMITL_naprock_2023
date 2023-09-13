1abb2a51e69744771fece2b5dcec6bf9ba9fb06d
IngCr1mson <61595501+inginglucky7@users.noreply.github.com>
2023-09-14 02:54:19 +0700

    randomizeCV

--- classified_func/KFold.py
+++ classified_func/KFold.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import StratifiedKFold, GridSearchCV, cross_val_score
+from sklearn.model_selection import StratifiedKFold, GridSearchCV, cross_val_score, RandomizedSearchCV
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from scipy.stats import randint
 import pickle
 
 df = pd.read_csv('../recognition/coords.csv')
@@ -13,20 +14,24 @@ y = df['class']
 
 rf = RandomForestClassifier(random_state=1234)
 
-param_grid = {
-    'n_estimators': [50, 100, 200, 400],
+param_dist = {
+    'n_estimators': randint(50, 400),
     'max_depth': [None, 10, 20, 30],
-    'min_samples_split': [2, 5, 10, 20],
-    'min_samples_leaf': [1, 2, 4, 8]
+    'min_samples_split': randint(2, 20),
+    'min_samples_leaf': randint(1, 8)
 }
 
 cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=1234)
 
-grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv=cv, scoring='accuracy', n_jobs=-1)
+#Gridsearch
+# grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv=cv, scoring='accuracy', n_jobs=-1)
 
-grid_search.fit(x, y)
+random_search = RandomizedSearchCV(estimator=rf, param_distributions=param_dist, n_iter=10, cv=cv, scoring="accuracy", n_jobs=-1, random_state=1234)
 
-best_rf = grid_search.best_estimator_
+# grid_search.fit(x, y)
+random_search.fit(x, y)
+
+best_rf = random_search.best_estimator_
 
 cv_accuracy = cross_val_score(best_rf, x, y, cv=cv, scoring='accuracy')
 print("Cross-Validation Accuracy:", np.mean(cv_accuracy))
