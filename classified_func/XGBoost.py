80fb90ed9452b4908dda8c40e2bd0e11cfa6b517
IngCr1mson <61595501+inginglucky7@users.noreply.github.com>
2023-09-20 04:24:03 +0700

    XGBoost fixed y column are not correct data

--- classified_func/XGBoost.py
+++ classified_func/XGBoost.py
@@ -9,13 +9,21 @@ import pickle
 df = pd.read_csv('../recognition/coords.csv')
 
 # Separate features (x) and target (y)
+class_label_mapping = {
+    'Arm': 0,
+    'Calves': 1,
+    'Feet': 2,
+    'Fist': 3,
+    'HDown': 4,
+    'Thighs': 5
+}
+
 x = df.drop('class', axis=1)
 y = df['class']
+y = y.map(class_label_mapping)
 
-# Initialize the XGBoost classifier
 xgb = XGBClassifier(random_state=1234)
 
-# Define a parameter distribution for hyperparameter tuning
 param_dist = {
     'n_estimators': np.arange(50, 401, 50),  # Range of estimators
     'max_depth': np.arange(3, 7),  # Range of max_depth
@@ -24,28 +32,17 @@ param_dist = {
     'colsample_bytree': [0.6, 0.7, 0.8, 0.9],  # Fraction of features used by trees
 }
 
-# Initialize StratifiedKFold for cross-validation
 cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=1234)
 
-# Initialize RandomizedSearchCV with XGBoost, parameter distribution, and cross-validation
 random_search = RandomizedSearchCV(estimator=xgb, param_distributions=param_dist, n_iter=10, cv=cv, scoring="accuracy", n_jobs=-1, random_state=1234)
 
-# Fit RandomizedSearchCV to find the best hyperparameters
-try:
+random_search.fit(x, y)
 
-    random_search.fit(x, y)
-
-except Exception as e:
-    print(e)
-# Get the best XGBoost estimator from RandomizedSearchCV
 best_xgb = random_search.best_estimator_
 
-# Calculate cross-validation accuracy
 cv_accuracy = cross_val_score(best_xgb, x, y, cv=cv, scoring='accuracy')
 
-# Print the mean cross-validation accuracy
 print("Cross-Validation Accuracy:", np.mean(cv_accuracy))
 
-# Save the best XGBoost model to a file
 with open('body_language_kFold_xgb.pkl', 'wb') as f:
     pickle.dump(best_xgb, f)
